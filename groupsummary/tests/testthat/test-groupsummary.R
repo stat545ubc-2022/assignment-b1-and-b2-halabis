a7ec23440ac0dfc56f914b0c1c46554b89d0ebec
halabis <halabis2@student.ubc.ca>
2022-11-16 23:24:56 -0800

    added data

--- groupsummary/tests/testthat/test-groupsummary.R
+++ groupsummary/tests/testthat/test-groupsummary.R
@@ -1,8 +1,8 @@
 test_that("multiplication works", {
-  expect_error(summary_function(datateachr::vancouver_trees, tree_id, genus_name))
-  expect_equal((summary_function(datateachr::cancer_sample, diagnosis, radius_mean)),
-               (datateachr::cancer_sample %>%
+  expect_error(summary_function(vancouver_trees, tree_id, genus_name))
+  expect_equal((summary_function(cancer_sample, diagnosis, radius_mean)),
+               (cancer_sample %>%
                   dplyr::group_by(diagnosis) %>%
                   dplyr::summarise(n=dplyr::n(), mean = mean(radius_mean), median = median(radius_mean), min = min(radius_mean), max = max(radius_mean), sd = sd(radius_mean))))
-  expect_true(tibble::is_tibble(summary_function(datateachr::cancer_sample, diagnosis, radius_mean)))
+  expect_true(tibble::is_tibble(summary_function(cancer_sample, diagnosis, radius_mean)))
 })
